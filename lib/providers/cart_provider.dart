1ba68c9ada4312425a229e32478ee2b679a95364
Chaineze Said <chachanam24@gmail.com>
2026-05-09 19:26:29 +0200

    modication persistance Panier

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -7,44 +7,52 @@ class CartProvider with ChangeNotifier {
   final CartDatabaseService _dbService;
 
   List<CartItem> _items = [];
-  
+  bool _isLoading = false;
 
   List<CartItem> get items => _items;
-  
+  bool get isLoading => _isLoading;
   int get itemCount => _items.length;
 
-  bool estDansLePanier(int productId) {
+  bool  estDansLePanier(int productId) {
     return _items.any((item) => item.product.id == productId);
   }
 
   CartProvider({CartDatabaseService? dbService})
-      : _dbService = dbService ?? CartDatabaseService();
-
-  Future<void> init() async {
-    _items = await _dbService.getCart();
-    notifyListeners();
-    
+      : _dbService = dbService ?? CartDatabaseService() {
+    _chargerPanier();
   }
 
+  Future<void> _chargerPanier() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      _items = await _dbService.getCart();
+    } catch (_) {
+      _items = [];
+    }
+    _isLoading = false;
+    notifyListeners();
+  }
 
   Future<void> ajouterProduit(Product product) async {
-    final index = _items.indexWhere((i) => i.product.id == product.id,);
-    if (index >= 0) {
+
+    final index = _items.indexWhere((i) => i.product.id == product.id);
+
+     if (index >= 0) {
       _items[index].quantity++;
     } else {
-      _items.add(CartItem(product: product),);
+      _items.add(CartItem(product: product));
     }
-    
-    await _dbService.saveCart(_items,);
-      
+
+    await _dbService.saveCart(_items);
     notifyListeners();
-}
+  }
 
   Future<void> retirerProduit(int productId) async {
-      _items.removeWhere((i) => i.product.id == productId);
+    _items.removeWhere((i) => i.product.id == productId);
 
-      await _dbService.saveCart(_items);
-
-      notifyListeners();
-    }
+    await _dbService.saveCart(_items);
+    notifyListeners();
+  }
 }
